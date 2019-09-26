1e4a3bbdb3f28ad8f9386bb78294003e955b6b91
Saswata Chowdhury <55692138+saswata-chowdhury@users.noreply.github.com>
2019-09-26 22:53:44 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,81 +1,105 @@
-  var interval;
-  document.getElementById("start").addEventListener("click", function(){
-    //var mydata = JSON.parse(TABLE_DATA);
-   
-    interval = window.setInterval(function(){
-        addRows();
-      }, 1000);
-  });
+class RandomDataDisplay{
 
-  document.getElementById("stop").addEventListener("click", function(){
-    clearInterval(interval);
-   
-});
-
-function deleteRow(tableRef){
-    if(tableRef.getElementsByTagName("tr").length > 0){
-        for(var j = 0; j< TABLE_DATA.length; j++){
-            console.log(tableRef.deleteRow(j))
-            //tableRef.deleteRow(j);
-        }
+    constructor(timeinterval, tableRefVal){
+        this.timeinterval = timeinterval;
+        this.intervalObject = null;
+        this.tableRef = document.getElementById(tableRefVal).getElementsByTagName('tbody')[0];
+        this.randArray = [];
     }
+
+    addRandomRows(){
+        this.intervalObject = window.setInterval(()=>{
+            this.randArray = this.suffleArray(TABLE_DATA); 
+            this.addRows(this.tableRef, this.randArray);
+        },  this.timeinterval);
+    }
+
+    addRows(tableObject, array){
+        this.deleteRows();      
+        array.forEach(function(val){ 
+            // Insert a row in the table at the last row
+            let newRow   = tableObject.insertRow();
     
-}
-
-function addRows(){
-
-    var tableRef = document.getElementById('dataTable').getElementsByTagName('tbody')[0];
-    deleteRow(tableRef);
-    
-    var randArray = [];
-
-    while(randArray.length < TABLE_DATA.length){ 
-        var random_index = Math.floor(Math.random() * TABLE_DATA.length);
-        if(randArray.indexOf(random_index) === -1){ console.log(randArray.length);
-            randArray.push(random_index);
-             // Insert a row in the table at the last row
-        var newRow   = tableRef.insertRow();
-
-        // Insert a cell in the row at index 0
-        var newCell  = newRow.insertCell(0);
-   
-        // Append a text node to the cell
-        var newText  = document.createTextNode(TABLE_DATA[random_index]["id"]);
-        newCell.appendChild(newText);
-
-        // Insert a cell in the row at index 0
-        var newCell  = newRow.insertCell(1);
-   
-        // Append a text node to the cell
-        var newText  = document.createTextNode(preventXSS(TABLE_DATA[random_index]["thumbnailUrl"]));
-        newCell.appendChild(newText);
-
-
-        // Insert a cell in the row at index 0
-        var newCell  = newRow.insertCell(2);
-   
-        // Append a text node to the cell
-        var newText  = document.createTextNode(preventXSS(TABLE_DATA[random_index]["name"]));
-        newCell.appendChild(newText);
-
-
-        // Insert a cell in the row at index 0
-        var newCell  = newRow.insertCell(3);
-   
-        // Append a text node to the cell
-        var newText  = document.createTextNode(TABLE_DATA[random_index]["price"]);
-        newCell.appendChild(newText);
-        }
-
+            // Insert a cell in the row at index 0
+            let newCell  = newRow.insertCell(0);
+       
+            // Append a text node to the cell
+            let newText  = document.createTextNode(val.id);
+            newCell.appendChild(newText);
+            // Insert a cell in the row at index 1
+            newCell  = newRow.insertCell(1);
+       
+            // Append a text node to the cell
+            newText  = document.createTextNode(val.name);
+            newCell.appendChild(newText);
         
+            // Insert a cell in the row at index 2
+            newCell  = newRow.insertCell(2);
+       
+            // Append a text node to the cell
+            newText  = document.createTextNode(val.thumbnailUrl);
+            newCell.appendChild(newText);
+        
+            // Insert a cell in the row at index 3
+            newCell  = newRow.insertCell(3);
+       
+            // Append a text node to the cell
+            newText  = document.createTextNode(val.price);
+            newCell.appendChild(newText);
+    
+         }); 
     }
 
+    deleteRows(){
+        this.tableRef.innerHTML='';
+    }
+
+    stopDataChange(){
+        clearInterval(this.intervalObject);
+    }
+
+    suffleArray(array){
+        let currentIndex = array.length;
+        let temporaryValue;
+        let randomIndex;
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+        }
+
+        sortData(){
+            this.randArray = TABLE_DATA.sort(function(a, b){
+                return a.id-b.id
+            });
+
+            this.addRows(this.tableRef,this.randArray);
+        }
+    }
+    let timeintervalObj;
+    let randomDataObject = new RandomDataDisplay(1000, 'dataTable');
+    document.getElementById("start").addEventListener("click", () => {
+        randomDataObject.addRandomRows();
+      });
+
+      document.getElementById("stop").addEventListener("click", () =>{
+        randomDataObject.stopDataChange();
+       
+    });
+
+    document.getElementById("sort").addEventListener("click", () =>{
+        randomDataObject.sortData();
+       
+    });
     
-}
-
-function preventXSS(str){
-
-   var regex = /(<([^>]+)>)/ig
-    result = str.replace(regex, "");
-    return result;
-}
+    
--- json/data.js
+++ json/data.js
@@ -1,61 +1,61 @@
 var TABLE_DATA = [
   {
     "id": "5",
-    "name": "cony #5",
+    "name": "ABCD #5",
     "thumbnailUrl": "image/5.gif",
     "price": 170
   },
   {
     "id": "1",
-    "name": "cony #1",
+    "name": "DEFG #1",
     "thumbnailUrl": "image/1.gif",
     "price": 170
   },
   {
     "id": "2",
-    "name": "cony #2",
+    "name": "GHIJ #2",
     "thumbnailUrl": "image/2.gif",
     "price": 270
   },
   {
     "id": "8",
-    "name": "cony #8",
+    "name": "KLMN #8",
     "thumbnailUrl": "image/8.gif",
     "price": 70
   },
   {
     "id": "10",
-    "name": "<img onerror='window.document.body.innerHTML = \"<h1>XSS</h1>\";' src=''> ",
+    "name": "MNOP #5",
     "thumbnailUrl": "image/10.gif",
     "price": 170
   },
   {
     "id": "4",
-    "name": "cony #4",
+    "name": "QRST #4",
     "thumbnailUrl": "image/4.gif",
     "price": 150
   },
   {
     "id": "3",
-    "name": "cony #3",
+    "name": "UVWX #3",
     "thumbnailUrl": "image/3.gif",
     "price": 130
   },
   {
     "id": "6",
-    "name": "cony #6",
+    "name": "VWXY #6",
     "thumbnailUrl": "image/6.gif",
     "price": 160
   },
   {
     "id": "9",
-    "name": "cony #9",
+    "name": "WXYZ #9",
     "thumbnailUrl": "image/9.gif",
     "price": 170
   },
   {
     "id": "7",
-    "name": "cony #7",
+    "name": "DBCA #7",
     "thumbnailUrl": "image/7.gif",
     "price": 170
   }
